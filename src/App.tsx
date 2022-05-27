5147589255fb9d4a388c02a4f92980e42a2ea339
Roman Troian <89692499+RomanAlxndrvch@users.noreply.github.com>
2022-05-26 23:02:56 -0600

    useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,57 @@
 import React, {useState} from 'react';
 import classes from "./App.module.css";
-import {Button} from "./components/Button";
-import {Simulate} from "react-dom/test-utils";
+// import {Button} from "./components/Button";
+// import {Simulate} from "react-dom/test-utils";
 
 
 function App() {
-    let [a, setA] = useState(1)
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+    let [button, defineBtn] = useState('')
 
-    const onClickHandler = () => {
-        setA(++a)
-        console.log(a)
+    let currentMoney = money
+
+    if (button === 'Rubls') {
+        currentMoney = money.filter((el) => el.banknots === 'RUBLS')
+    }
+    if (button === 'Dollars') {
+        currentMoney = money.filter((el) => el.banknots === 'Dollars')
+    }
+    if (button === 'All') {
+        currentMoney = money
     }
 
-    const ZeroHandler = () => {
-        setA(a = 0)
+    ////////////////////////////////////////////
+
+    const onClickFilterHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const btn = e.currentTarget.innerText
+        defineBtn(button = btn)
+
+
     }
 
-    return (
-        <div className={classes.app}>
-            <h1>{a}</h1>
-            <button onClick={onClickHandler}>Number</button>
-            <button onClick={ZeroHandler}>0</button>
+    return (<div className={classes.app}>
+            <ul>
+                {currentMoney.map((el, index) =>
+                    <li key={index}>
+                        <span> {el.banknots} </span>
+                        <span> {el.value} </span>
+                        <span> {el.number} </span>
+                    </li>
+                )}
 
+            </ul>
+            <button onClick={onClickFilterHandler}>Dollars</button>
+            <button onClick={onClickFilterHandler}>Rubls</button>
+            <button onClick={onClickFilterHandler}>All</button>
         </div>
 
     )
@@ -29,3 +59,4 @@ function App() {
 
 
 export default App;
+
